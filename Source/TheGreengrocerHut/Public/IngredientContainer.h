3d12d8e0c4f3b7fd2af2ab421d50490a27cb0d45
Stepan-Malinovskii <step98070@gmail.com>
2026-06-13 00:02:31 +0300

    Refact Ingridient

--- Source/TheGreengrocerHut/Public/IngredientContainer.h
+++ Source/TheGreengrocerHut/Public/IngredientContainer.h
@@ -1,13 +1,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Engine/DataTable.h"
 #include "IngredientContainer.generated.h"
 
-USTRUCT(BlueprintType)
-struct FIngredientContainer : public FTableRowBase
+UCLASS(BlueprintType)
+class UIngredientContainer : public UDataAsset
 {
 	GENERATED_BODY()
 	
+public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UStaticMesh* Mesh;
 
@@ -16,4 +19,46 @@ struct FIngredientContainer : public FTableRowBase
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	USoundCue* SFX;
+};
+
+USTRUCT(BlueprintType)
+struct FTermsOfUse
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "-1", ClampMax = "2", UIMin = "-1", UIMax = "2"))
+	int AddPriority;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "1", ClampMax = "10", UIMin = "1", UIMax = "10"))
+	int Mixing;
+};
+
+USTRUCT(BlueprintType)
+struct FIngredient : public FTableRowBase
+{
+    GENERATED_BODY()
+
+    UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Ingredient")
+    FText DisplayName;
+
+    UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Ingredient", meta = (MultiLine = "true"))
+    FText Description;
+
+    UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Ingredient")
+    FLinearColor MainColor;
+
+    UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Ingredient")
+    TObjectPtr<UTexture2D> Icon;
+
+    UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Ingredient")
+    TObjectPtr<UStaticMesh> Mesh;
+
+    UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Ingredient")
+    TObjectPtr<USoundCue> SFX;
+
+    UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Ingredient")
+    TObjectPtr<UIngredientContainer> Container;
+
+    UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Ingredient")
+    FTermsOfUse TermsOfUse;
 };
