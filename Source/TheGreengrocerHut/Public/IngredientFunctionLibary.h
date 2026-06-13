8eafc5536681055cebdf24a126bbfe28a8dbbddc
Stepan-Malinovskii <step98070@gmail.com>
2026-06-13 23:56:28 +0300

    For Van

--- Source/TheGreengrocerHut/Public/IngredientFunctionLibary.h
+++ Source/TheGreengrocerHut/Public/IngredientFunctionLibary.h
@@ -11,21 +11,6 @@ class UIngredientFunctionLibary : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
-    UFUNCTION()
-    static TArray<FName> GetIngredientRowNames()
-    {
-        TArray<FName> Names;
-        UDataTable* Table = LoadObject<UDataTable>(nullptr, TEXT("/Game/Data/DT_Ingredients.DT_Ingredients"));
-
-        if (Table)
-        {
-            Names = Table->GetRowNames();
-        }
-
-        return Names;
-    }
-
-public:
     UFUNCTION(BlueprintCallable, Category = "Ingredient")
     static void GetTwoStrongestColors(const TArray<FIngredient>& Ingredients, FLinearColor& OutColor1, FLinearColor& OutColor2);
 };
