be3ee7de834c44cd2baac99f216d40eb0da3b2f5
Ivan Bashkov <bashkovia@mail.ru>
2026-06-20 02:13:10 +0300

    Converter. Help

---
+++ Source/TheGreengrocerHut/Public/Converter.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ConverterRecipe.h"
+#include "Converter.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class THEGREENGROCERHUT_API UConverter : public UPrimaryDataAsset
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    FText DisplayName;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    FText Description;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    TObjectPtr<UTexture2D> Icon;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    TObjectPtr<USkeletalMesh> Mesh;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    TObjectPtr<UAnimMontage> ActiveMontage;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    TObjectPtr<UAnimMontage> UseMontage;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    TObjectPtr<USoundCue> SFX;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    FTransform TargetPoint;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    FVector BoxExtent;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    TArray<FConverterRecipe> RecipeArray;
+};
