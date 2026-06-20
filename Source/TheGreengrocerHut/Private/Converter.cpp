ea0b0a36079ead8c549e5a252549941e8a1fb990
Ivan Bashkov <bashkovia@mail.ru>
2026-06-20 15:23:17 +0300

    Converter

--- Source/TheGreengrocerHut/Private/Converter.cpp
+++ Source/TheGreengrocerHut/Private/Converter.cpp
@@ -1,2 +1,21 @@
 #include "Converter.h"
 
+bool UConverter::ContainsRecipe(FName IngredientRowName) {
+    for (const auto& recipe : RecipeArray) {
+        if (recipe.From.RowName == IngredientRowName) {
+            return true;
+        }
+    }
+    return false;
+}
+
+FConverterRecipe UConverter::GetRecipe(FName IngredientRowName) {
+    if (ContainsRecipe(IngredientRowName)) {
+        for (const auto& recipe : RecipeArray) {
+            if (recipe.From.RowName == IngredientRowName) {
+                return recipe;
+            }
+        }
+    }
+    return { NAME_None, NAME_None, 0 };
+}
