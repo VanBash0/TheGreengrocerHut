493f1dc10a629c7e65b802a156f66919b1cb7638
Stepan-Malinovskii <step98070@gmail.com>
2026-06-23 00:33:14 +0300

    BOok

--- Source/TheGreengrocerHut/Private/GameLoop.cpp
+++ Source/TheGreengrocerHut/Private/GameLoop.cpp
@@ -33,8 +33,6 @@ bool UGameLoop::ShouldCreateSubsystem(UObject* Outer) const
     FString WorldName = World->GetName();
 
 #if WITH_EDITOR
-    if (WorldName.Contains(TEXT("LOBBY_LEVEL")))
-        return false;
     return true;
 #else
     if (WorldName.Contains(TEXT("GAME_LEVEL")))
