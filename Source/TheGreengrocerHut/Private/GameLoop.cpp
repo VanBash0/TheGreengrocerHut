a5500e0a1fa99cef24a8224fffd48adce2ebcc4e
Ivan Bashkov <bashkovia@mail.ru>
2026-07-30 10:26:01 +1000

    фикс чего-то

--- Source/TheGreengrocerHut/Private/GameLoop.cpp
+++ Source/TheGreengrocerHut/Private/GameLoop.cpp
@@ -111,6 +111,46 @@ void UGameLoop::IncrementCurrentClient()
                 [this]()
                 {
                     currentClientIndex_++;
+
+                    // Логирование симптомов
+                    if (_currentDaySnapshot.DayClients.IsValidIndex(currentClientIndex_))
+                    {
+                        const FClient& Client = _currentDaySnapshot.DayClients[currentClientIndex_];
+
+                        FString SymptomsString;
+                        for (const FName& Symptom : Client.Symptoms)
+                        {
+                            SymptomsString += Symptom.ToString() + TEXT(", ");
+                        }
+
+                        if (!SymptomsString.IsEmpty())
+                        {
+                            SymptomsString.LeftChopInline(2); // убрать последнюю ", "
+                        }
+                        else
+                        {
+                            SymptomsString = TEXT("No symptoms");
+                        }
+
+                        // Print String
+                        if (GEngine)
+                        {
+                            GEngine->AddOnScreenDebugMessage(
+                                -1,
+                                8.f,
+                                FColor::Green,
+                                FString::Printf(TEXT("Client %d: %s"),
+                                    currentClientIndex_,
+                                    *SymptomsString)
+                            );
+                        }
+
+                        // И в Output Log тоже
+                        UE_LOG(LogTemp, Warning, TEXT("Client %d symptoms: %s"),
+                            currentClientIndex_,
+                            *SymptomsString);
+                    }
+
                     SetNewState(EGameState::SpawnClient);
                 },
                 SpawnDelay,
